d9e2e6ff439fcfce23b3e08566dd629dd94b7891
Muhammad Talha <talha424588@gmail.com>
2024-09-18 15:32:34 +0500

    remove pusher code to reduce exception from ws and pusher server & remove hardcoded values from seee by

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -17,26 +17,26 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-import Echo from 'laravel-echo';
+// import Echo from 'laravel-echo';
 
-import Pusher from 'pusher-js';
+// import Pusher from 'pusher-js';
 
-window.Pusher = Pusher;
+// window.Pusher = Pusher;
 
-window.Echo = new Echo({
+// window.Echo = new Echo({
 
-    broadcaster: 'pusher',
+//     broadcaster: 'pusher',
 
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
 
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
 
-    wsHost: window.location.hostname,
+//     wsHost: window.location.hostname,
 
-    wsPort: 6001,
+//     wsPort: 6001,
 
-    forceTLS: false,
+//     forceTLS: false,
 
-    disableStats: true,
+//     disableStats: true,
 
-});
+// });
