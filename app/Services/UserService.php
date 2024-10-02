6a798ce8ba2ff84c38104d2f9a5e691e8adf3e80
Muhammad Talha <talha424588@gmail.com>
2024-10-02 16:32:13 +0500

    fcm token will now save in user table

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -7,6 +7,7 @@ use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 class UserService implements UserRepository
 {
@@ -14,7 +15,15 @@ class UserService implements UserRepository
 
     public function updateFcmToken($token)
     {
-        return $token;
+        $user = User::findorFail(Auth::user()->id);
+        if($user)
+        {
+            $user->fcm_token = $token;
+            if($user->save())
+                return response()->json(["status"=>true,"message"=>"success"],200);
+            else
+                return response()->json(["status"=>false,"message"=>"something went wrong"],500);
+        }
     }
     // public function authenticateUser(LoginRequestBody $request)
     // {
