3e5d2086384278c70556ac3139ea6e53d305458f
Muhammad Talha <talha424588@gmail.com>
2024-11-27 16:53:04 +0500

    remove unread count function from controller & change unread count function accessor

--- app/Http/Controllers/Chat/GroupController.php
+++ app/Http/Controllers/Chat/GroupController.php
@@ -37,33 +37,4 @@ class GroupController extends Controller
     {
         return $this->groupRepository->fetchGroupLastMessage($groupId);
     }
-
-    public function getUserUnreadMessageCount()
-{
-    $groupDetails = []; // Initialize an array to store all group details
-    $groups = Group::whereRaw("FIND_IN_SET(?, REPLACE(access, ' ', '')) > 0", [Auth::user()->id])
-        ->with(['UserAllgroupMessages' => function ($query) {
-            $query->latest('time')
-                ->whereNot('status', EnumMessageEnum::MOVE);
-        }, 'UserAllgroupMessages.user'])
-        ->get();
-
-    foreach ($groups as $group) {
-        $counter = 0; // Reset counter for each group
-        foreach ($group->UserAllgroupMessages as $groupMessage) {
-            $seenBy = explode(",", $groupMessage->seen_by);
-            if (!in_array(Auth::user()->unique_id, $seenBy)) {
-                $counter++; // Increment counter for each unread message
-            }
-        }
-            $groupDetails[] = [
-                "name" => $group->name,
-                "group_id" => $group->group_id,
-                "unread_count" => $counter
-            ];
-    }
-
-    return $groupDetails; // Return all group details
-}
-
 }
