7ec1868638b5e0c9362dba00fd58b4f385392fed
Muhammad Talha <talha424588@gmail.com>
2024-11-28 16:33:30 +0500

    remove unnecessary code

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -10,39 +10,6 @@ use Illuminate\Support\Facades\Http;
 
 class FirebaseService
 {
-
-    // public function sendNotification(Request $request)
-    // {
-    //     $senderName = $request->input('senderName', 'Default Sender');
-    //     $messageContent = $request->input('message', 'Default Message');
-    //     $subsIds = $request->input('subsIds');
-    //     $subsIdsArray = is_string($subsIds) ? json_decode($subsIds, true) : $subsIds;
-
-    //     $validSubscriptionIds = is_array($subsIdsArray) ? array_filter($subsIdsArray, function ($id) {
-    //         return !is_null($id);
-    //     }) : [];
-    //     $validSubscriptionIds = array_map('trim', $validSubscriptionIds);
-    //     Log::info('Valid Subscription IDs:', $validSubscriptionIds);
-    //     $fullMessage = "$senderName $messageContent";
-
-    //     $response = Http::withHeaders([
-    //         'Authorization' => 'Basic MGZjOWMzNTctMzlmOS00ZjMxLWE4MmUtNzIxOTkyZjFmYjhm',
-    //         'Accept' => 'application/json',
-    //         'Content-Type' => 'application/json',
-    //     ])->post('https://api.onesignal.com/notifications', [
-    //         'app_id' => 'd9ec86fd-fc8c-4567-8573-0428916eb93e',
-    //         'target_channel' => 'push',
-    //         'headings' => ['en' => 'Vm Chat'],
-    //         'contents' => ['en' => $fullMessage],
-    //         'include_subscription_ids' => ["8e57cc49-8fa1-46ed-aa0f-36f59541f792","8e57cc49-8fa1-46ed-aa0f-36f59541f791","8e57cc49-8fa1-46ed-aa0f-36f59541f793"],
-    //     ]);
-    //     Log::info('Valid Subscription IDs:', $validSubscriptionIds);
-    //     Log::info('OneSignal Response:', $response->json());
-    //     return $response->json();
-    // }
-
-
-
     public function sendNotification($message)
     {
         $senderName = $message->user->name;
