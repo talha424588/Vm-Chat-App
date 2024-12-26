d2a8d6141c909d676129a50a3e6d67b3ee52bf3f
hassanraza <rezveshah1211@gmail.com>
2024-12-26 09:49:12 +0500

    kaun talha 4.0

--- app/Http/Resources/MessageResourceCollection.php
+++ app/Http/Resources/MessageResourceCollection.php
@@ -14,69 +14,27 @@ class MessageResourceCollection extends ResourceCollection
      *
      * @return array<int|string, mixed>
      */
-    // public function toArray(Request $request)
-    // {
-    //     return $this->collection->map(function ($message) {
-    //         return [
-    //             'id' => $message->id,
-    //             'sender' => $message->sender,
-    //             'group_id' => $message->group_id,
-    //             'msg' => $message->msg,
-    //             'reply_id' => $message->reply_id,
-    //             'seen_by' => $message->seen_by,
-    //             'time' => $message->time,
-    //             'type' => $message->type,
-    //             'media_name' => $message->media_name,
-    //             'user' => [
-    //                 'id' => $message->user->id,
-    //                 'unique_id' => $message->user->unique_id,
-    //                 'name' => $message->user->name,
-    //                 'email' => $message->user->email,
-    //                 'role' => $message->user->role,
-    //                 'user_status' => $message->user->user_status,
-    //                 'email_status' => $message->user->email_status,
-    //                 'access' => $message->user->access,
-    //                 'chat_status' => $message->user->chat_status,
-    //                 'seen_privacy' => $message->user->seen_privacy,
-    //                 'code' => $message->user->code,
-    //                 'status' => $message->user->status,
-    //             ],
-    //         ];
-    //     });
-    // }
-    public function toArray(Request $request)
-{
-    return $this->collection->map(function ($message) {
-        $reply = $message->reply ? [
-            'id' => $message->reply->id,
-            'sender' => $message->reply->sender,
-            'group_id' => $message->reply->group_id,
-            'msg' => $message->reply->msg,
-            'type' => $message->reply->type,
-            'media_name' => $message->reply->media_name,
-            'reply_id' => $message->reply->reply_id,
-            'seen_by' => $message->reply->seen_by,
-            'time' => $message->reply->time,
-            'is_compose' => $message->reply->is_compose,
-            'is_privacy_breach' => $message->is_privacy_breach,
-            'is_deleted' => $message->is_deleted,
-        ] : null;
 
-        return [
-            'id' => $message->id,
-            'sender' => $message->sender,
-            'group_id' => $message->group_id,
-            'msg' => $message->msg,
-            'reply_id' => $message->reply_id,
-            'seen_by' => $message->seen_by,
-            'time' => $message->time,
-            'type' => $message->type,
-            'media_name' => $message->media_name,
-            'is_compose' => $message->is_compose,
-            'is_privacy_breach' => $message->is_privacy_breach,
-            'is_deleted' => $message->is_deleted,
-            'compose_id'=>$message->compose_id,
-            'user' => [
+    public function toArray(Request $request)
+    {
+        return $this->collection->map(function ($message) {
+            $reply = $message->reply ? [
+                'id' => $message->reply->id,
+                'sender' => $message->reply->sender,
+                'group_id' => $message->reply->group_id,
+                'msg' => $message->reply->msg,
+                'type' => $message->reply->type,
+                'media_name' => $message->reply->media_name,
+                'reply_id' => $message->reply->reply_id,
+                'seen_by' => $message->reply->seen_by,
+                'time' => $message->reply->time,
+                'is_compose' => $message->reply->is_compose,
+                'is_privacy_breach' => $message->is_privacy_breach,
+                'is_deleted' => $message->is_deleted,
+            ] : null;
+
+            // Check if user is not null
+            $user = $message->user ? [
                 'id' => $message->user->id,
                 'unique_id' => $message->user->unique_id,
                 'name' => $message->user->name,
@@ -90,12 +48,29 @@ class MessageResourceCollection extends ResourceCollection
                 'code' => $message->user->code,
                 'status' => $message->user->status,
                 'fcm_token' => $message->user->fcm_token,
-                'pic'=>$message->user->profile_img,
-            ],
-            'reply' => $reply,
-        ];
-    });
-}
+                'pic' => $message->user->profile_img,
+            ] : null;
+
+            return [
+                'id' => $message->id,
+                'sender' => $message->sender,
+                'group_id' => $message->group_id,
+                'msg' => $message->msg,
+                'reply_id' => $message->reply_id,
+                'seen_by' => $message->seen_by,
+                'time' => $message->time,
+                'type' => $message->type,
+                'media_name' => $message->media_name,
+                'is_compose' => $message->is_compose,
+                'is_privacy_breach' => $message->is_privacy_breach,
+                'is_deleted' => $message->is_deleted,
+                'compose_id' => $message->compose_id,
+                'user' => $user,  // Only add user data if it exists
+                'reply' => $reply,
+            ];
+        });
+    }
+
 
     public function withResponse($request, $response)
     {
