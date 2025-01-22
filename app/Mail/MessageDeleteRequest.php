6d80180462c93fca424ee55c538727a81e80a4e8
Muhammad Talha <talha424588@gmail.com>
2025-01-22 11:20:32 +0500

    remove un used class from message delete template

--- app/Mail/MessageDeleteRequest.php
+++ app/Mail/MessageDeleteRequest.php
@@ -2,16 +2,10 @@
 
 namespace App\Mail;
 
-use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Date;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Str;
 
 class MessageDeleteRequest extends Mailable
 {
