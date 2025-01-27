2d07737ed0d897c7c57150a6f02fb8b0aa737bd4
Muhammad Talha <talha424588@gmail.com>
2025-01-27 12:26:28 +0500

    update server event for save message & append message from visamtion

--- server.js
+++ server.js
@@ -14,40 +14,47 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
     console.log('connection');
 
-    socket.on('sendChatToServer', async (msg) => {
+    socket.on('sendChatToServer', async (msg, isSaved) => {
         console.log("message details", msg);
-        const response = await axios.post('http://127.0.0.1:8000/messages', msg);
-        console.log("resposer", response);
-        if (response.status == 201) {
-            const savedMessage = response.data;
-
-            socket.emit('sendChatToClient', savedMessage);
-            socket.broadcast.emit('sendChatToClient', savedMessage);
-
-            console.log('Message stored successfully!');
-        } else {
-            console.error('Error storing message:', response.status);
+        if (isSaved == true) {
+            socket.emit('sendChatToClient', msg);
+            socket.broadcast.emit('sendChatToClient', msg);
         }
+        else {
+            const response = await axios.post('http://127.0.0.1:8000/messages', msg);
+            console.log("resposer", response);
+            if (response.status == 201) {
+                const savedMessage = response.data;
+
+                socket.emit('sendChatToClient', savedMessage);
+                socket.broadcast.emit('sendChatToClient', savedMessage);
+
+                console.log('Message stored successfully!');
+            } else {
+                console.error('Error storing message:', response.status);
+            }
+        }
+
     });
 
     // Server-side code
-    socket.on('deleteMessage', (message,isMove) => {
+    socket.on('deleteMessage', (message, isMove) => {
         console.log("delete message event", message);
-        io.emit('deleteMessage', message,isMove);
+        io.emit('deleteMessage', message, isMove);
         io.emit('updateGroupMessages', message);
     });
 
-    socket.on('moveMessage', (moveMessages, newGroupId, groupId,uniqueId) => {
-        io.emit('moveMessage', moveMessages, newGroupId, groupId,uniqueId);
+    socket.on('moveMessage', (moveMessages, newGroupId, groupId, uniqueId) => {
+        io.emit('moveMessage', moveMessages, newGroupId, groupId, uniqueId);
     });
 
     socket.on('updateEditedMessage', (editedMessage) => {
         io.emit('updateEditedMessage', editedMessage);
     });
 
-    socket.on('restoreMessage', (message,uniqueId) => {
-        console.log("Restore message event", message,uniqueId);
-        io.emit('restoreMessage', message,uniqueId);
+    socket.on('restoreMessage', (message, uniqueId) => {
+        console.log("Restore message event", message, uniqueId);
+        io.emit('restoreMessage', message, uniqueId);
     })
 
     socket.on('disconnect', () => {
